99191ca6394775d90801a72598b20d0d5643e308
Bronnikov Nikita <spader332@gmail.com>
2020-10-02 13:04:55 +0300

    initial commit

---
+++ DevExpressTest/DevExpressTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace CandleStick2DChart
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+    public class ChartViewModel
+    {
+        public ObservableCollection<DataPoint> Data { get; private set; }
+        public ChartViewModel()
+        {
+            Random random = new Random();
+            Data = new ObservableCollection<DataPoint> ();
+            for(int i = 0; i < 10000; i++)
+            {
+
+                var time = new DateTime(2019, 1, 1).AddMinutes(i);
+                Data.Add(new DataPoint(time, random.Next(24,33), random.Next(24, 33), random.Next(24, 33), random.Next(24, 33)));
+            }
+        }
+    }
+    public class DataPoint
+    {
+        public DateTime Date { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public DataPoint(DateTime arg, double open, double high, double low, double close)
+        {
+            this.Date = arg;
+            this.Open = open;
+            this.High = high;
+            this.Low = low;
+            this.Close = close;
+        }
+    }
+}
